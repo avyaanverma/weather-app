b3a39bad27f8511341f777a995c3b2b011349658
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-08-20 22:14:16 +0530

    weather app created

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-async function fetchData() {
+async function fetchData(city) {
 
-  const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Chandigarh`;
+  const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
 
   const options = {
     method: 'GET',
@@ -13,10 +13,48 @@ async function fetchData() {
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    const dataObj = result;
-    console.log(dataObj);
+    
+
+    const temp = document.querySelector('.temp');
+
+    if(response || temp.innerHTML != undefined){
+    temp.innerText = `${result['temp']}°C`;
+
+    const cityElement = document.querySelector('.city');
+    cityElement.innerText = city;
+
+    const humidity = document.querySelector('.humidity1');
+    humidity.innerText = `Humidity: ${result['humidity']}%`;
+
+    const wind = document.querySelector('.wind1');
+    wind.innerText = `Wind: ${result['wind_speed']}km/h`;}
+    else{
+      const temp = document.querySelector('.temp');
+    temp.innerText = `Please enter correct city Name`;
+    }
+    
+
+
+
+    console.log(result);
   } catch (error) {
     console.error(error);
+    const temp = document.querySelector('.temp');
+    temp.innerText = `Please enter correct city Name`;
   }
 }
 
+
+search = document.getElementById
+
+city = document.querySelector('#input');
+searchBtn = document.querySelector('.searchpng');
+searchBtn.addEventListener("click", ()=>{
+  fetchData(city.value)
+})
+fetchData("Chandigarh");
+
+
+
+
+// city = "Kanpur";
